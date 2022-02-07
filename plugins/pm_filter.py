5963e0dfdc421d384716c73ea3b5636fda47e6a8
Ajithkannan1234 <90961342+Ajithkannan1234@users.noreply.github.com>
2022-02-07 19:45:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -692,8 +692,9 @@ async def advantage_spell_chok(msg):
     movielist += [(re.sub(r'(\-|\(|\)|_)', '', i, flags=re.IGNORECASE)).strip() for i in gs_parsed]
     movielist = list(dict.fromkeys(movielist)) # removing duplicates
     if not movielist:
-        k = await msg.reply("I couldn't find anything related to that. Check your spelling  [msg me🥴](https://t.me/Filmhunters345_bot) m")
-        await asyncio.sleep(8)
+        k = await msg.reply("I couldn't find anything related to that. Check your spelling  [MSG ME🥴](https://t.me/Filmhunters345_bot) ")
+        disable_web_page_preview=True
+        await asyncio.sleep(60)
         await k.delete()
         return
     SPELL_CHECK[msg.message_id] = movielist
