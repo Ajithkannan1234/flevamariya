f9b71e88fbb74287f8908bf905fc50a2679c24ec
Ajithkannan1234 <90961342+Ajithkannan1234@users.noreply.github.com>
2022-02-17 18:30:47 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,53 +1,30 @@
-class Script(object):
-    START_TXT = """Hello {},
+class script(object):
+    START_TXT = """Hey 
 
-My name is <a href=https://t.me/{}>{}</a>!
-
-ഈ ബോട്ട് @Filmhuters123 ഗ്രൂപ്പിലേക്ക് ഉള്ളത് എന്ന് ഇനി വീണ്ടും വീണ്ടും പറയണോ??
+ഈ ബോട്ട് @moviecafe234 ഗ്രൂപ്പിലേക്ക് ഉള്ളത് എന്ന് ഇനി വീണ്ടും വീണ്ടും പറയണോ??
 അപ്പോ പിന്നെ എന്തിനാ വീണ്ടും വീണ്ടും സ്റ്റാർട്ട് കുത്തി കളിക്കാൻ വരുന്നേ... ആ സൈഡിലോട്ട് എങ്ങാനും മാറി ഇരിക്ക്‌ ഇനി🤭🤭
 
-@Filmhunters123"""
-
-    HELP_TXT = """Hey {}
-Podey podey......"""
-
-    ABOUT_TXT = """<b>➥ My name: {}
-➥ Creator:Ajith"""
-
-    ADMIN_TXT = """Help: <b>Admin Mods</b>
-
-<b>NOTE:</b>
-This module only works for my admins
-
-<b>Commands and Usage:</b>
-• /logs - to get the rescent errors.
-• /stats - to get status of files in db.
-• /delete - to delete a specific file from db.
-• /users - to get list of my users and ids.
-• /chats - to get list of the my chats and ids.
-• /leave - to leave from a chat.
-• /disable - do disable a chat.
-• /ban_users - to ban a user.
-• /unban_users - to unban a user.
-• /channel - to get list of total connected channels.
-• /broadcast - to broadcast a message to all users."""
-
-    STATUS_TXT = """<b>Total Files:</b> <code>{}</code>
-<b>Total Users:</b> <code>{}</code>
-<b>Total Chats:</b> <code>{}</code>
-<b>Used Storage:</b> <code>{}</code> MiB
-<b>Free Storage:</b> <code>{}</code> MiB"""
-
-    FORCESUB_TXT = """**🚫READ THIS INSTRUCTION🚫 \n \n 🗣നിങ്ങൾ ചോദിക്കുന്ന സിനിമകൾ നിങ്ങൾക്ക് ലഭിക്കണം എന്നുണ്ടെങ്കിൽ നിങ്ങൾ താഴെ കൊടുത്തിട്ടുള്ള ചാനലിൽ ചാനലിൽ ജോയിൻ ചെയ്യണം. ജോയിൻ ചെയ്ത ശേഷം TRY AGAIN പ്രെസ്സ് ചെയ്താൽ  MOVIE ലഭിക്കും😍 \n \n 🗣 In Order In Order To Get The Movie Requested By You in Our Groups, You Will Have To Join Our Official Channel First. After That, PRESS Try AGAIN BUTTON..I'll Send You That Movie Privately 🙈 \n \n 👇 JOIN THIS CHANNEL & PRESS TRY AGAIN**"""
-
+@moviecafe234"""
+    HELP_TXT = """𝙷𝙴y"""
+    ABOUT_TXT = """✯"""
+    SOURCE_TXT = """🤣"""
+    MANUELFILTER_TXT = """Help"""
+    BUTTON_TXT = """Help"""
+    AUTOFILTER_TXT = """Help"""
+    CONNECTION_TXT = """Help"""
+    EXTRAMOD_TXT = """Hel"""
+    ADMIN_TXT = """He"""
+    STATUS_TXT = """★ 𝚃𝙾𝚃𝙰𝙻 𝙵𝙸𝙻𝙴𝚂: <code>{}</code>
+★ 𝚃𝙾𝚃𝙰𝙻 𝚄𝚂𝙴𝚁𝚂: <code>{}</code>
+★ 𝚃𝙾𝚃𝙰𝙻 𝙲𝙷𝙰𝚃𝚂: <code>{}</code>
+★ 𝚄𝚂𝙴𝙳 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱
+★ 𝙵𝚁𝙴𝙴 𝚂𝚃𝙾𝚁𝙰𝙶𝙴: <code>{}</code> 𝙼𝚒𝙱"""
     LOG_TEXT_G = """#NewGroup
 Group = {}(<code>{}</code>)
 Total Members = <code>{}</code>
 Added By - {}
 """
-
     LOG_TEXT_P = """#NewUser
 ID - <code>{}</code>
-Name - {}"""
-
-    
+Name - {}
+"""
