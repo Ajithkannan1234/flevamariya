8b8276a79c2956eabceb6351fd256591b7e85a1e
Ajithkannan1234 <90961342+Ajithkannan1234@users.noreply.github.com>
2022-02-18 19:27:57 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -39,7 +39,7 @@ async def next_page(bot, query):
 
     ident, req, key, offset = query.data.split("_")
     if int(req) not in [query.from_user.id, 0]:
-        return await query.answer("oKda", show_alert=True)
+        return await query.answer("There is something wrong \n\n 1-May Be <b>Spelling Mistake<\b> \n 2-May be Movie is not Released or Still in Theaters \n 3-May be that movie is not our database", show_alert=True)
     try:
         offset = int(offset)
     except:
