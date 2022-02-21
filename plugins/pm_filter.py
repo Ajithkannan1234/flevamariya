b66310973c1070762d3766e178b2c4639ee0f1cd
Ajithkannan1234 <90961342+Ajithkannan1234@users.noreply.github.com>
2022-02-21 22:47:40 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -130,14 +130,14 @@ async def advantage_spoll_choker(bot, query):
             hmm = InlineKeyboardMarkup(
         [
             [
-                 InlineKeyboardButton("🔎 Search On Google 🔍", url="https://google.com/search")
-                 ],[
                  InlineKeyboardButton("Msg Me🥴", url="https://t.me/Testing363738_bot")
+                 ],[
+                 InlineKeyboardButton()"🔎 Search On Google 🔍", url="https://www.imdb.com/search/"
             ]
         ]
     )      
-            k = await query.message.edit('please check release date and spelling🙏 \n \n ഗൂഗിളിൽ പൊയി സ്പെല്ലിങ്ങും❗️RELEASE❗️ഡേറ്റും CHECK ചെയുക🥴 \n \n രണ്ടും ശെരി ആയിട്ടും Movie കിട്ടിയില്ലെങ്കിൽ മാത്രം', reply_markup=hmm)
-            await asyncio.sleep(20)
+            k = await query.message.edit('please check release date & spelling🙏 \n \n [MSG ME](https://t.me/Testing363738_bot) Only if you don't get the movie even though both are true \n \n രണ്ടും ശെരി ആയിട്ടും Movie കിട്ടിയില്ലെങ്കിൽ മാത്രം [MSG ME👇](https://t.me/Testing363738_bot)', reply_markup=hmm)
+            await asyncio.sleep(30)
             await k.delete()
 
 
