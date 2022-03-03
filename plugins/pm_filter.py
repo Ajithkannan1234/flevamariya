5365ba151f0e25d4a2b74bf014045ef9a479f930
Ajithkannan1234 <90961342+Ajithkannan1234@users.noreply.github.com>
2022-03-03 21:56:56 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -624,7 +624,7 @@ async def auto_filter(client, msg, spoll=False):
             [InlineKeyboardButton(text="Join Our Channel😌❤️",url="https://t.me/+BQP56d3IH0piYjM1")]
         )
         btn.insert(0,
-            [InlineKeyboardButton(text="join channel",url"https://t.me/+txtUUj8amJIyZjA1"), InlineKeyboardButton(text="join",url="https://t.me/moviecafe234")]  
+            [InlineKeyboardButton(text="join channel",url="https://t.me/+txtUUj8amJIyZjA1"), InlineKeyboardButton(text="join",url="https://t.me/moviecafe234")]  
 
     reply_id = message.reply_to_message.message_id if message.reply_to_message else message.message_id
     imdb = await get_poster(search, file=(files[0]).file_name) if IMDB else None
